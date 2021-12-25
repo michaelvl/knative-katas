e3edce02dd32db707b4360b02f719fd016cbc5d7
MichaelVL <mvl.gh@network42.dk>
2021-12-25 15:05:27 +0100

    Add data option to curler

--- scripts/curler.sh
+++ scripts/curler.sh
@@ -2,9 +2,17 @@
 
 URL=$1
 REQUESTS=${2:-1}
+DATA=${3:-""}
+
+if [ -z "$DATA" ]; then
+    DATA_ARG=""
+else
+    DATA_ARG="-d $DATA"
+fi
 
 ii=0
 while [ $ii -lt $REQUESTS ]; do
-    curl -s -X POST $URL | jq --compact-output .
+    UUID=$(uuidgen)
+    curl -s -X POST $DATA_ARG -H "content-type: application/json" -H "ce-specversion: 1.0" -H "ce-source: curl" -H "ce-type: example" -H "ce-id: $UUID" $URL | jq --compact-output .
     let ii=ii+1
 done
