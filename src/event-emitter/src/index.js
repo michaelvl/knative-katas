1f19b9791f45e7ddc4e41b250d3e136e536ddf30
MichaelVL <mvl.gh@network42.dk>
2021-12-29 06:48:07 +0100

    Make event type and source configurable in event emitter

--- src/event-emitter/src/index.js
+++ src/event-emitter/src/index.js
@@ -7,6 +7,8 @@ const hostname = os.hostname();
 const data = process.env.APP_DATA || '{"msg": "Hello world!"}';
 const cron_schedule = process.env.APP_CRON_SCHEDULE || '*/1 * * * *';
 const sink_url = process.env.K_SINK;
+const event_type = process.env.APP_EVENT_TYPE || 'type-example';
+const event_source = process.env.APP_EVENT_SOURCE || 'simple-example';
 
 console.log('Sink URL', sink_url);
 
@@ -16,7 +18,7 @@ process.on('SIGTERM', () => {
 })
 
 cron.schedule(cron_schedule, () => {
-    const ce = new CloudEvent({ type: 'type-example', source: 'source-example', data });
+    const ce = new CloudEvent({ type: event_type, source: event_source, data });
     const message = HTTP.binary(ce); // Or HTTP.structured(ce)
 
     console.log('Sending event', ce.id);
