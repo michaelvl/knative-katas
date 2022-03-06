f2d6b706c9ff82f785e5e0e8333bfad9bf5837e3
MichaelVL <mvl.gh@network42.dk>
2022-03-06 09:49:14 +0100

    Fix image update when using digests

--- ci/set-client-image-tag.sh
+++ ci/set-client-image-tag.sh
@@ -16,11 +16,12 @@ fi
 IMAGE=ghcr.io/michaelvl/knative-katas
 
 DIGEST=$($SCRIPTPATH/../scripts/skopeo.sh inspect docker://$IMAGE:$TAG | jq -r .Digest)
+echo "Using digest: $DIGEST"
 
 #IMAGE_TAG=":$TAG"
 IMAGE_TAG="@$DIGEST"
 
 for m in deploy/*.yaml; do
     echo "Updating $m"
-    sed -i -E "s#(^\s+-\s+image\:\s+$IMAGE)\:.*#\1$IMAGE_TAG#" $m
+    sed -i -E "s#(^\s+-\s+image\:\s+$IMAGE)[\:@]{1}.*#\1$IMAGE_TAG#" $m
 done
